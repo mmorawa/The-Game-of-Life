91d95ca2c0dd989f53314c463cd166c78e7855c9
mmorawa <32575904+mmorawa@users.noreply.github.com>
2019-06-01 18:22:33 +0200

    Add data validation

--- The-Game-Of-Life/Form2.cs
+++ The-Game-Of-Life/Form2.cs
@@ -20,7 +20,37 @@ namespace The_Game_Of_Life
 
         private void Button_OK_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (BoardLength.Text == "" || StartPercentage.Text == "" || NumberOfCycles.Text == "")
+                {
+                    MessageBox.Show("You must provide a value for all fields.");
+                    return;
+                }
+                else if (int.Parse(BoardLength.Text) <= 0 || int.Parse(StartPercentage.Text) <= 0 || int.Parse(NumberOfCycles.Text) <= 0)
+                {
+                    MessageBox.Show("Each value must be above 0.");
+                    return;
+                }
+                else if (int.Parse(StartPercentage.Text) >= 100)
+                {
+                    MessageBox.Show("You cannot exceed 100%.");
+                    return;
+                }
+                else
+                {
+                    Length = int.Parse(BoardLength.Text);
+                    Percentage = int.Parse(StartPercentage.Text);
+                    Cycle = int.Parse(NumberOfCycles.Text);
 
+                    DialogResult = DialogResult.OK;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Input data is in wrong format.");
+                return;
+            }
 
 
         }
